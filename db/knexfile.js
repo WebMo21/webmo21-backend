f0068ea219b4f04ab7f85f7fd2819dd380b19397
Sascha Majewsky <sascha.majewsky@pm.me>
2021-06-24 22:00:18 +0200

    Added initial db migration file

--- db/knexfile.js
+++ db/knexfile.js
@@ -13,6 +13,10 @@ module.exports = {
     },
     migrations: {
       tableName: "knex_migrations",
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
     },
   },
 };
